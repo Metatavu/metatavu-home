3e6d53f15e8eb8eefccdf14fb7ca2600996a02bd
riku mikkonen <riku.mikkonen5@gmail.com>
2024-12-12 10:07:49 +0200

    add new consent-utils file

--- src/utils/consent-utils.tsx
+++ src/utils/consent-utils.tsx
@@ -1,18 +1,18 @@
 import SettingsScreen from "src/components/screens/settings-screen";
 
 const ConstentUtils = ({userId}: {userId: string}) => {
-  const keycloakService = ();
+//const keycloakService = ();
 
-  const handleSave = async (isAccepted: boolean) => {
-      try {
-          await keycloakService.updateUserAttribute(userId, "consentGiven", isAccepted.toString());
-          console.log("Tiedot tallennettu onnistuneesti Keycloakiin!");
-      } catch (error) {
-          console.error("Virhe tallennuksessa:", error);
-      }
-  };
+const handleSave = async (isAccepted: boolean) => {
+    try {
+        await keycloakService.updateUserAttribute(userId, "consentGiven", isAccepted.toString());
+        console.log("Tiedot tallennettu onnistuneesti Keycloakiin!");
+    } catch (error) {
+        console.error("Virhe tallennuksessa:", error);
+    }
+};
 
-  return <SettingsScreen onSave={handleSave} />;
+return <SettingsScreen onSave={handleSave} />;
 };
 
 export default ConstentUtils
