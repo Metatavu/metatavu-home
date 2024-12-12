e9590ee89f5329e02b85b038b9a6a5b870b15d60
riku mikkonen <riku.mikkonen5@gmail.com>
2024-12-12 09:57:17 +0200

    add consent-popup file not {testing}

---
+++ src/components/screens/consent-popup.tsx
@@ -0,0 +1,31 @@
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
+
+type ConsentDialogProps = {
+    open: boolean; 
+    onAccept: () => void; 
+    onDecline: () => void;  
+  };
+
+  const ConsentPopUp: React.FC<ConsentDialogProps> = ({ open , onAccept, onDecline}) => {
+    return (
+      <Dialog open={open} maxWidth="sm" fullWidth>
+        <DialogTitle>Suostumus tietojen käsittelyyn</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">
+            Sovellus tarvitsee suostumuksesi tietojen käsittelyyn, jotta voit käyttää tiettyjä toimintoja, kuten
+            sprinttinäkymää ja tasapainokorttia. Hyväksytkö tietojen käsittelyn?
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onAccept}   variant="contained" color="primary">
+            Hyväksy
+          </Button>
+          <Button onClick={onDecline} color="secondary">
+            En hyväksy
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  
+  export default ConsentPopUp;
