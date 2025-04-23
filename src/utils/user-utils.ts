97fbb39811578510314473c31bd4e6f84d282183
Lauri Marttinen <dlama008@edu.xamk.fi>
2025-04-23 09:29:22 +0300

    Remove unused imports and removed error handling in getSeveraUserId function, so user doesn't get error screen if they are missing SeveraUserId

--- src/utils/user-utils.ts
+++ src/utils/user-utils.ts
@@ -1,8 +1,6 @@
 import type { User } from "src/generated/homeLambdasClient/models/User";
 import config from "src/app/config";
-import strings from "src/localization/strings";
-import { useSetAtom } from "jotai";
-import { errorAtom } from "src/atoms/error";
+
 
 /**
  * To get the severa user id when logged in, if severaUserId is not found and in develop mode, it will return the test user severa id
@@ -11,14 +9,12 @@ import { errorAtom } from "src/atoms/error";
  * @returns user severaUserId or testUserSeveraId
  */
 export const getSeveraUserId = (user: User | undefined): string => {
-  //const setError = useSetAtom(errorAtom);
   const severaUserId = user?.attributes?.severaUserId;
 
   if (!severaUserId) {
     if (import.meta.env.MODE === "development") {
       return config.user.testUserSeveraId ?? "";
     }
-    //setError(strings.error.noSeveraUserId);
     return "";
   }
   return severaUserId;
