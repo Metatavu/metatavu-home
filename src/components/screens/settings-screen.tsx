298ce084eded58c2dfc4beddc53e588d2a6f2707
Aleksandra Balashova <ext-aleksandra.balashova@metatavu.fi>
2025-05-15 10:53:44 +0300

    Fix Severa opt-in toggle logic and update attribute flow

--- src/components/screens/settings-screen.tsx
+++ src/components/screens/settings-screen.tsx
@@ -17,30 +17,35 @@ const SettingsScreen = () => {
   const [isConsentGiven, setIsConsentGiven] = useState(
     Boolean(userProfile?.attributes?.severaUserId)
   );
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleToggleChange = () => {
     grantSeveraOptInConsent();
   };
 
   const grantSeveraOptInConsent = async () => {
-      try {
-        if (!userProfile?.email || !userProfile?.id) {
-          setError(strings.error.missingEmailOrId);
-          return;
-        }
-
-        const response = await usersApi.updateUserAttribute({
-          updateUserAttributeRequest: { email: userProfile?.email },
-          id: userProfile?.id,
-          attributeName: "severaUserId"
-        });
-
-        setIsConsentGiven(Boolean(response.updatedKeycloakAttributes?.severaUserId));
-      } catch (error) {
-        console.error("Error fetching consent:", error);
-        setError(`${strings.error.fetchFailedFlextime}, ${error}`);
+    setIsLoading(true);
+    try {
+      if (!userProfile?.email || !userProfile?.id) {
+        setError(strings.error.missingEmailOrId);
+        return;
       }
-    };
+
+      const response = await usersApi.updateUserAttribute({
+        updateUserAttributeRequest: { email: userProfile?.email },
+        id: userProfile?.id,
+        attributeName: "isSeveraOptIn"
+      });
+
+      setIsConsentGiven(Boolean(response.updatedKeycloakAttributes?.severaUserId));
+    } catch (error) {
+      console.error("Error fetching consent:", error);
+      setError(`${strings.error.fetchFailedFlextime}, ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
 
   return (
     <Box p={2} bgcolor="grey.100" borderRadius={2}>
@@ -55,7 +60,7 @@ const SettingsScreen = () => {
           checked={isConsentGiven}
           onChange={handleToggleChange}
           inputProps={{ "aria-label": "information" }}
-          disabled={isConsentGiven}
+          disabled={isConsentGiven || isLoading}
         />
         <Typography variant="body1" sx={{ marginLeft: 2 }}>
           {strings.settingsScreen.accept}
