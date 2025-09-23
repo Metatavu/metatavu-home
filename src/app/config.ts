03e93e454bfae7d6ea71687a792efcd5933ac9c4
Alexander-K-byte <alexanderkerr@hotmail.com>
2025-09-23 16:14:26 +0300

    reverted config file to check for issues

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,6 +6,9 @@ interface Config {
     realm: string;
     clientId: string;
   };
+  api: {
+    baseUrl: string;
+  };
   lambdas: {
     baseUrl: string;
   };
@@ -28,6 +31,7 @@ const env = cleanEnv(import.meta.env, {
   VITE_KEYCLOAK_URL: url(),
   VITE_KEYCLOAK_REALM: str(),
   VITE_KEYCLOAK_CLIENT_ID: str(),
+  VITE_API_BASE_URL: url(),
   VITE_FORECAST_USER_ID_OVERRIDE: num({ default: undefined }),
   VITE_HOME_LAMBDAS_BASE_URL: url(),
   VITE_SEVERA_TEST_USER_ID: str({ default: undefined }),
@@ -41,6 +45,9 @@ const config: Config = {
     url: env.VITE_KEYCLOAK_URL,
     realm: env.VITE_KEYCLOAK_REALM,
     clientId: env.VITE_KEYCLOAK_CLIENT_ID,
+  },
+  api: {
+    baseUrl: env.VITE_API_BASE_URL,
   },
   lambdas: {
     baseUrl: env.VITE_HOME_LAMBDAS_BASE_URL,
