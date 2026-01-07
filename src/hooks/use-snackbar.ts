40ca76e0ac08007de7a96a9e8d67be2f37213246
Alexander-K-byte <alexanderkerr@hotmail.com>
2026-01-07 14:31:15 +0200

    removed unused types from hook

--- src/hooks/use-snackbar.ts
+++ src/hooks/use-snackbar.ts
@@ -4,7 +4,7 @@ import { snackbarAtom } from "src/atoms/snackbar";
 export const useSnackbar = () => {
   const setSnackbar = useSetAtom(snackbarAtom);
 
-  return (message: string, severity: "success" | "error" | "info" | "warning" = "success") => {
+  return (message: string, severity: "success" | "error" = "success") => {
     setSnackbar({ open: true, message, severity });
   };
 };
