a162447b8fb34e300ad9ddaba0046d19ff60cf02
Eelis96 <eelis.lantinen14@gmail.com>
2026-08-12 14:41:28 +0300

    update homescreen 'customize' button colour

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -130,7 +130,7 @@ const HomeScreen = () => {
       <Box id="home-screen" display="flex" flexDirection="column" alignItems="end">
         <Box>
           <AppButton
-            variant="primary"
+            variant="tertiary"
             text={editmode ? strings.label.cancel : strings.label.customize}
             startIcon={!editmode && <EditOutlined />}
             onClick={() => handleEdit()}
